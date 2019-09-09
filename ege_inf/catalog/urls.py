9e0b91a265849e04b0bd30c87efe5636335d1814
RomanovMaxim <romanovmxm@gmail.com>
2019-09-09 23:45:47 +0300

    Created View for Lesson

--- ege_inf/catalog/urls.py
+++ ege_inf/catalog/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     #path('', views.IndexView.as_view(), name='index'),
     path('lesson/', views.LessonView.as_view(), name='lesson'),
     #path('lesson/', views.lesson, name='lesson'),
+    #path('course/', views.course, name='course'),
+    path('course/', views.CourseView.as_view(), name='course'),
+    path('lesson/<int:pk>/', views.lesson_detail, name='lesson_detail'),
 ]
--- ege_inf/catalog/views.py
+++ ege_inf/catalog/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
-from .models import Topic, Task
+from .models import Topic, Task, Course, Lesson, LessonTasks
 
 # def index(request):
 #     latest_task_list = Task.objects.all()
@@ -27,5 +27,22 @@ class LessonView(generic.ListView):
     def get_queryset(self):
        return Task.objects.all()
 
-# def lesson(request):
-#     return render(request, 'catalog/lesson.html',{})
+def lesson_detail(request, pk):
+    lesson_task_list = LessonTasks.objects.filter(lesson=pk)
+    latest_task_list = []
+    for lesson_task in lesson_task_list:
+        task = get_object_or_404(Task, pk=lesson_task.task.pk)
+        latest_task_list.append(task)
+    return render(request, 'catalog/lesson.html', context={'latest_task_list':latest_task_list,})
+
+
+
+class CourseView(generic.ListView):
+    template_name = 'catalog/course.html'
+    context_object_name = 'lesson_list'
+
+    def get_queryset(self):
+       return Lesson.objects.all()
+
+# def course(request):
+#     return render(request, 'catalog/course.html',{})
